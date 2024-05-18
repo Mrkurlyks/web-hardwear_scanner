a2d9f1d0f3e81aa9fc00a8f4288b6247b8ed4db1
Mrkurlyks <xoma.123@hotmail.com>
2024-05-18 21:58:04 +0300

    web

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-from flask import Flask, render_
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1> ты пес <h1> "
+
+if __name__ == "__main__":
+    app.run(debug=True)
